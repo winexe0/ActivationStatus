80586980413c6fc05772f5b53d9c7c1d97ad773a
aryan <47309918+winexe0@users.noreply.github.com>
2020-10-07 13:16:39 -0700

    Update ActivationStatus.cpp

--- ActivationStatus.cpp
+++ ActivationStatus.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <windows.h>
 int main() {
+   system("title ActivationStatus v1.0.4 Console");
    MessageBox(0,"Please Wait", "Please Wait", MB_OK);
    system("slmgr /xpr");
 }
