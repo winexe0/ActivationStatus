13289ec1a148361103a26bf934fde9a2450cd404
aryan <47309918+winexe0@users.noreply.github.com>
2020-10-13 13:57:19 -0700

    Update ActivationStatus.cpp

--- ActivationStatus.cpp
+++ ActivationStatus.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 int main() {
    system("title ActivationStatus v1.0.4 Console");
+   system("echo Detected Processor is %PROCESSOR_ARCHITECTURE%");
    MessageBox(0,"Please Wait", "Please Wait", MB_OK);
    system("slmgr /xpr");
 }
