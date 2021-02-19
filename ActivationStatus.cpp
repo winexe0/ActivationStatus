cec03370fb2661f25cc00ae6a7fac10cd6cb40ba
winexe0 <47309918+winexe0@users.noreply.github.com>
2021-02-19 09:24:26 -0800

    Update ActivationStatus.cpp

--- ActivationStatus.cpp
+++ ActivationStatus.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <windows.h>
 #include <string>
+#include <VersionHelpers.h>
 using namespace std;
 int main() {
    string confirm;
-   system("title ActivationStatus v1.2.0 Console");
+   system("title ActivationStatus v1.2.1 Console");
    system("echo Detected Processor is %PROCESSOR_ARCHITECTURE%");
+    if (!IsWindowsVistaOrGreater())
+    {
+       MessageBox(NULL, "ActivationStatus requires at least Windows Vista to function", "Windows Version Not Supported", MB_ICONSTOP | MB_OK);
+       cout << "Exiting due to Windows version not supported" << endl;
+       exit(1);
+    }
    int license = MessageBox(
         NULL,
         "Do you accept the license terms?\nIt can be found at https://github.com/winexe0/ActivationStatus/blob/master/LICENSE ",
